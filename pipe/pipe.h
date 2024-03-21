f0cdd315375a8d2342aeddfa987a13c48fc9b93c
Setu Gupta <setu@tamu.edu>
2024-03-21 18:09:03 -0500

    Added code and test case
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   pipe/pipe.h
    	modified:   test/main.cc

--- pipe/pipe.h
+++ pipe/pipe.h
@@ -1,5 +1,4 @@
 #include <condition_variable>
-#include <iostream>
 #include <mutex>
 #include <optional>
 #include <queue>
@@ -14,7 +13,6 @@ namespace multipipe
                         bool                    done = false; // This marks the end of transaction
                         std::queue<int>         q;            // The shared queue
                         std::mutex              m;            // Mutex used to access the queue
-                        std::mutex              cvpm;         // Mutex for waking up the producer
                         std::condition_variable cv_producer;  // Conditional variable for waking up producer
                         std::condition_variable cv_consumer;  // Conditional variable for waking up consumer
 
@@ -28,46 +26,59 @@ namespace multipipe
                         Pipe(const Pipe&)            = delete;
                         Pipe& operator=(const Pipe&) = delete;
 
-                        explicit Pipe();
-                        ~Pipe();
+                        Pipe() {}
         };
-
-        template<typename T, size_t N>
-        Pipe<T, N>::Pipe()
-        {
-                std::cout << "Constructed Pipe" << std::endl;
-        }
 
         template<typename T, size_t N>
         void Pipe<T, N>::transmit_data(T&& data)
         {
-                std::cout << "Transmitting " << data << std::endl;
+                std::unique_lock lk(m);
+                if(q.size() > N) // Wait for the queue to drain
+                        cv_producer.wait(lk);
+
+                q.push(data);
+                cv_consumer.notify_all();
+                // std::cout << "Sent " << data << std::endl;
         }
 
         template<typename T, size_t N>
         bool Pipe<T, N>::finish_transmission()
         {
-                std::cout << "Terminating transmission" << std::endl;
-                return false;
+                std::unique_lock lk(m);
+                if(done) // Return false if the pipe was already closed
+                        return false;
+                done = true;
+                cv_consumer.notify_all();
+                return true;
         }
 
         template<typename T, size_t N>
         bool Pipe<T, N>::is_empty()
         {
-                std::cout << "Checking for data" << std::endl;
-                return false;
+                std::unique_lock lk(m);
+                return q.size() == 0;
         }
 
         template<typename T, size_t N>
         std::optional<T> Pipe<T, N>::receive_data()
         {
-                std::cout << "Fetching data" << std::endl;
+                std::unique_lock lk(m);
+
+                if(done && q.size() == 0)
+                        return std::nullopt;
+
+                if(!done && q.size() == 0) // Wait for the queue to fill up
+                        cv_consumer.wait(lk);
+
+                if(q.size() != 0)
+                {
+                        std::optional<T> ret = std::make_optional<T>(q.front());
+                        q.pop();
+                        cv_producer.notify_all();
+                        return ret;
+                }
+
                 return std::nullopt;
         }
 
-        template<typename T, size_t N>
-        Pipe<T, N>::~Pipe()
-        {
-                std::cout << "Destructed Pipe" << std::endl;
-        }
 } // namespace multipipe
--- test/main.cc
+++ test/main.cc
@@ -1,12 +1,13 @@
+#include <iomanip>
 #include <iostream>
 #include <pipe.h>
 #include <thread>
 
-#define PIPE_SIZE 3
+#define PIPE_SIZE (int)1e3
 
 void produce(multipipe::Pipe<int, PIPE_SIZE>& p)
 {
-        for(int i = 0; i < 10; i++) p.transmit_data(std::forward<int>(i));
+        for(int i = 0; i < 1e7; i++) p.transmit_data(std::forward<int>(i));
         p.finish_transmission();
 }
 
@@ -17,7 +18,7 @@ void consume(multipipe::Pipe<int, PIPE_SIZE>& p)
                 std::optional<int> data = p.receive_data();
                 if(!data.has_value())
                         break;
-                std::cout << "Got " << data.value();
+                std::cout << "Got " << std::setfill('0') << std::setw(10) << data.value() << std::endl;
         }
 }
 
